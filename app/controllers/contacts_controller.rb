05d13cd0f9647baf3392ea57a8bf533a9cfbd2cf
Josh Schneider <avipedia1986@gmail.com>
2015-06-15 18:21:24 -0400

    Added create action to Contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,22 @@
 class ContactsController < ApplicationController
-  
   def new
     @contact = Contact.new
   end
-  
+
   def create
-    
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: 'Your message has been sent.'
+    else
+      redirect_to new_contact_path, notice: 'Something went wrong; please try again.'
+    end
+
   end
-  
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :comments)
+  end
+
 end
