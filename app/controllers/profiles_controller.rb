a91b815532c5492dc09c9e1e7c7da465abe6215c
Josh Schneider <avipedia1986@gmail.com>
2015-07-02 17:57:54 -0400

    Added before_filters to check user id

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,8 @@
 class ProfilesController < ApplicationController
+  
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
   def new
     @user = User.find(params[:user_id])
     @profile = Profile.new
@@ -39,6 +43,11 @@ class ProfilesController < ApplicationController
 
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+  end
+  
+  def only_current_user
+    @user = User.find(params[:user_id])
+    redirect_to root_url unless @user == current_user
   end
 
 end
