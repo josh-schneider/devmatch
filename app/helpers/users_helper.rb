6777e57243c78a00652e7d789d1706a89d3c4b6c
Josh Schneider <avipedia1986@gmail.com>
2015-07-03 16:21:11 -0400

    Made improvements to user profile page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+
+  def job_title_icon
+    case @user.profile.job_title
+    when 'Developer' then '<i class="fa fa-code"></i>'.html_safe
+    when 'Entrepreneur' then '<i class="fa fa-lightbulb-o"></i>'.html_safe
+    when 'Investor' then '<i class="fa fa-dollar"></i>'.html_safe
+    else '<i class="fa fa-user"></i>'.html_safe
+    end
+  end
+  
+  def html_of(raw_text)
+    ("<p>" + raw_text.gsub(/\s*\n+\s*/,'</p><p>') + "</p>").html_safe
+  end
+
 end
