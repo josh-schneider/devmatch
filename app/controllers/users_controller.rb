5cd4271aef6fd3f35257932385626aca60e98a50
Josh Schneider <avipedia1986@gmail.com>
2015-07-05 09:42:27 -0400

    Improved user index and show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    
+    @users = User.includes(:profile).order('profiles.last_name')
   end
 
   def show
